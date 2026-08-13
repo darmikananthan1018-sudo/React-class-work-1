11aa66979871aad726173f2aabc38896d81870f5
darmika <darmikananthan1018@gmail.com>
2026-08-13 22:07:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Section from './components/Section';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Section />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+// Stylesheets
+import './assets/css/animate.css';
+import './assets/css/flex-slider.css';
+import './assets/css/fontawesome.css';
+import './assets/css/owl.css';
+import './assets/css/templatemo-tale-seo-agency.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
